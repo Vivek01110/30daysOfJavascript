5f050137a0cfd75ad42ed0824dc8b4e329b91726
Vivek01110 <vivek134679kumar@gmail.com>
2024-07-20 17:04:02 +0530

    Added Day1 file

---
+++ Day1.js
@@ -0,0 +1,58 @@
+// Task 1 :
+var myvar;
+myvar = 34;
+console.log(myvar);
+
+// Task 2 :
+let str;
+str = "vivek";
+console.log(str);
+
+
+// Task 3 :
+
+const ask = true;
+console.log(ask);
+
+
+// Task 4 :
+
+let arr= [2,4,3,7];
+
+
+
+console.log("type of myvar : ",typeof(myvar));
+console.log("type of str : ",typeof(str));
+console.log("type of ask : ",typeof(ask));
+
+for(let i = 0;i<4;i++)
+    console.log(arr[i]);
+
+class student{
+     name = "stylish";
+     age = 4;
+
+     constructor(name ,age){
+        this.name = name;
+        this.age = age;
+     }
+};
+
+let s1 =  new student("vivek",30);
+
+console.log("s1 name : ",s1.name);
+console.log("s2 age : ",s1.age);
+
+
+
+// Task 5 : reassigninig variable   
+
+let i  = 5;
+i = 3;
+
+console.log(i);
+
+// Task 6 : Const
+
+const j = 6;
+// j = 9;   //error
