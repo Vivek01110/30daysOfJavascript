3977442bcf2caef55251518af1e9a611d40a18eb
Vivek01110 <vivek134679kumar@gmail.com>
2024-07-20 19:00:00 +0530

    Added Day2 file

---
+++ Day2.js
@@ -0,0 +1,185 @@
+//OPERATORS
+
+// Activity 1 : Arithmatics Operations
+
+
+// Task 1 : Add Two Number 
+
+let a = 5;
+let b = 6;
+
+console.log("a + b = ", a + b);
+// output : 11 
+
+
+// Task 2 : Substract Two Number 
+
+let c = 5;
+let d = 6;
+
+console.log("c - d = ", a - b);
+// output : -1 
+
+
+// Task 3 : Multiply Two Number 
+
+let e = 5;
+let f = 6;
+
+console.log("e x f = ",e * f);
+// output : 30 
+
+
+// Task 4 : Divide Two Number 
+
+let g = 30;
+let h = 5;
+
+console.log("g / h = ",g / h);
+// output : 6
+
+
+// Task 5 : Divide Two Number 
+
+let x = 36;
+let y = 5;
+
+console.log("x % y = ",x % y);
+// output : 1
+
+ x = 30
+
+ console.log("x % y = ",x % y);
+//  ouptut : 0
+
+
+
+// Activity 2 : Assginment Operator
+
+
+// Task 6 :  += Operator
+
+ x = 8;
+ x += 2;
+ console.log("Addtion : ",x);
+//  Output : 10
+
+
+ // Task 7 :  -= Operator
+
+ x = 8;
+ x -= 2;
+ console.log("substraction : ",x);
+ //  Output : 6
+
+
+
+ x = 100; y = 50;
+
+//  Task 8 : > and < operator
+
+if(x>y)
+    console.log("x is greater than y");
+else if(x<y)
+    console.log("x is less than y");
+else 
+    console.log("x is equal to y");
+
+//  Task 9 : >= and <= operator
+
+if(x>=y)
+    console.log("x is greater or equal to than y");
+else 
+    console.log("x is less than y");
+
+// Task 10 : 
+//     == Operator 
+
+x=5;
+y=5;
+console.log(x==y);
+// Outout : true
+
+m = "1";
+n = 1;
+
+console.log(m===n);
+// Output : false
+
+
+
+// Task 11 :
+
+let num = 7; 
+if(num >=0 && num <= 9){
+    console.log("num has one digit");
+}
+else {
+    console.log("num has more than one digit");
+}
+
+
+
+// Task 12 :
+
+ num = 40; 
+if(num%2==0 || num %4==0){
+    console.log("num is even");
+}
+else {
+    console.log("num is odd");
+}
+
+// Task 13 : 
+
+num = 40; 
+if(!(num%2==0) || !(num %4==0)){
+    console.log("num is odd");
+}
+else {
+    console.log("num is even");
+}
+
+
+// Task 14 : Ternary Operator
+num = 5;
+let ispositive = (num>=0) ? true : false;
+console.log(ispositive);
+// output : true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
